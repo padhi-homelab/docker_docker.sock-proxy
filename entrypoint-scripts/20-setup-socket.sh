31136c38eb6c622183eff5f4bb14db220963f9b4
Saswat Padhi <saswatpadhi@protonmail.com>
2024-07-17 21:56:02 -0700

    uhh

--- entrypoint-scripts/20-setup-socket.sh
+++ entrypoint-scripts/20-setup-socket.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-socat UNIX-LISTEN:/tmp/docker.user.sock,fork,user=$DOCKER_USER,group=$DOCKER_GROUP,mode=777 \
+socat UNIX-LISTEN:/haproxy/docker.user.sock,fork,user=$DOCKER_USER,group=$DOCKER_GROUP,mode=777 \
       UNIX-CONNECT:/var/run/docker.sock &
